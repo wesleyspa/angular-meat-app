c21d1b226935da0aba04b2d6cd1401ea6d9c3170
Wesley Alves <wesleyspa@yahoo.com.br>
2018-03-20 21:59:52 -0300

    Menu Item and shopping cart animations

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready'
 
   @Input() menuItem: MenuItem
   @Output() add = new EventEmitter()
@@ -15,7 +27,7 @@ export class MenuItemComponent implements OnInit {
   ngOnInit() {
   }
 
-  emitAddEvent() {    
-    this.add.emit(this.menuItem)    
+  emitAddEvent() {
+    this.add.emit(this.menuItem)
   }
 }
--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from 'app/restaurants/restaurant/Restaurant.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
+        animate('300ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
+
 export class RestaurantComponent implements OnInit {
 
   @Input() restaurant: Restaurant
+
+  restaurantState = 'ready'
 
   constructor() { }
 
